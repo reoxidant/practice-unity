29aa50be4994186a6693bfc7f124eace0377354a
Виталий Шаповалов <vitalijsapovalov@MacBook-Pro-Vitalij.local>
2020-05-07 19:46:19 +0300

    [ADD] new Project Block Breaker + new scenes + scripts + sprites + plugin TMP

---
+++ 2D Course/Block Breaker/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour 
+{
+	public void LoadNextScene()
+	{
+		int currentSceneIndex = SceneManager.GetActiveScene ().buildIndex;
+		SceneManager.LoadScene (currentSceneIndex + 1);
+	}
+
+	public void LoadStartMenu()
+	{
+		SceneManager.LoadScene (0);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
