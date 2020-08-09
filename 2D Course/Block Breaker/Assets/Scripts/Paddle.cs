a7097d501528ae8be0cea628f5fe25f25cd4ba1b
Vitaliy Shapovalov <kupper133@gmail.com>
2020-08-09 11:42:22 +0300

    [ADD] new features for block breaker

---
+++ 2D Course/Block Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	[SerializeField] float minXpos = 1f;
+	[SerializeField] float maxXpos = 15f;
+	[SerializeField] float screenWidthInUnits = 16f;
+	private float mousePositionInUnits;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+		Vector2 paddlePosition = new Vector2 (transform.position.x, transform.position.y);
+		paddlePosition.x = Mathf.Clamp (mousePositionInUnits, minXpos, maxXpos);
+		transform.position = paddlePosition;
+	}
+}
