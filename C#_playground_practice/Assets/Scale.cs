74729c105d58788415cd4efec6bd15036560753f
Vitaliy Shapovalov <kupper133@gmail.com>
2020-08-09 12:03:28 +0300

    [ADD] project c# playgrounds and game NewLife

---
+++ C#_playground_practice/Assets/Scale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scale : MonoBehaviour {
+
+	void OnMouseDown ()
+	{
+		transform.localScale = new Vector3 (transform.localScale.x / 2, transform.localScale.y / 2, transform.localScale.z / 2);		
+	}
+
+	void OnTriggerEnter(Collider other){
+		print (other.gameObject.name);
+	}
+}
---
+++ C#_playground_practice/Assets/Script_6.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_6 : MonoBehaviour {
+
+	public GameObject obj;
+	private GameObject inst_obj;
+	[SerializeField]
+	private float speed = 4f;
+
+	// Use this for initialization
+	void Start () {
+		inst_obj = Instantiate (obj, new Vector3(0,0,0), Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float zPos = Input.GetAxis ("Vertical");
+
+		inst_obj.transform.Translate (Vector3.forward * speed * zPos * Time.deltaTime);
+	}
+}
